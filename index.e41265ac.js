61146f325d4abcd785f8b26b6b0195edf21c887a
Weector <108277280+Weector@users.noreply.github.com>
2023-03-01 20:15:13 +0000

    Deploying to gh-pages from @ Weector/Vintage@94678eb0fe2af101e0e25c254cfb5e58da3b34f9 🚀

---
+++ index.e41265ac.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".menu"),t=document.querySelector(".burger-container");e.addEventListener("click",(()=>{e.classList.toggle("active"),t.classList.toggle("burger-container-is-hidden")}));
+//# sourceMappingURL=index.e41265ac.js.map
